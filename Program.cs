7574129de368cfb41b2c94183266d5a40e221319
FABRICIO <fnmamao@gmail.com>
2020-03-02 21:21:56 -0300

    adiciona contagem

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercicio_contagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("preparar...");
+            Console.ReadKey();
+            Console.WriteLine("apontar...");
+            Console.ReadKey();
+            Console.WriteLine("atirar...");
+        }
+    }
+}
